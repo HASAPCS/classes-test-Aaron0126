b1e3cd174a8bf9d9ec95907a0eb3b3112e4a75e3
Aaron0126 <aaron.lin@has.hc.edu.tw>
2023-10-06 13:27:30 +0800

    I miss type a "s"

--- src/main/java/com/example/Fruit.java
+++ src/main/java/com/example/Fruit.java
@@ -7,8 +7,35 @@ public class Fruit {
     private double weight;
 
     public Fruit(String name, String color, String taste, double weight) {
+        this.name=name;
+        this.color=color;
+        this.taste=taste;
+        this.weight=weight;
         // TODO: Initialize the attributes
     }
-
+    public String getName(){
+        return this.name;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public String getTaste(){
+        return this.taste;
+    }
+    public double getWeight(){
+        return this.weight;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void setTaste(String taste){
+        this.taste=taste;
+    }
+    public void setWeight(double weight){
+        this.weight=weight;
+    }
     // TODO: Implement getters, setters, and any other methods you find necessary
 }
